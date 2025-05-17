07f2fcc10ee5d7d13df3adc201553c67ea50bf78
12999516 <12999516@itisrossi.vi.it>
2025-05-17 09:46:15 +0200

    a

---
+++ es/es/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace es
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_parola = new System.Windows.Forms.Label();
+            this.txt_guess = new System.Windows.Forms.TextBox();
+            this.lbl_messaggio = new System.Windows.Forms.Label();
+            this.btn_guess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbl_parola
+            // 
+            this.lbl_parola.Location = new System.Drawing.Point(24, 128);
+            this.lbl_parola.Name = "lbl_parola";
+            this.lbl_parola.Size = new System.Drawing.Size(228, 48);
+            this.lbl_parola.TabIndex = 0;
+            // 
+            // txt_guess
+            // 
+            this.txt_guess.Location = new System.Drawing.Point(15, 42);
+            this.txt_guess.Name = "txt_guess";
+            this.txt_guess.Size = new System.Drawing.Size(127, 22);
+            this.txt_guess.TabIndex = 1;
+            // 
+            // lbl_messaggio
+            // 
+            this.lbl_messaggio.Location = new System.Drawing.Point(12, 9);
+            this.lbl_messaggio.Name = "lbl_messaggio";
+            this.lbl_messaggio.Size = new System.Drawing.Size(130, 30);
+            this.lbl_messaggio.TabIndex = 2;
+            this.lbl_messaggio.Text = "inserisci una parola";
+            // 
+            // btn_guess
+            // 
+            this.btn_guess.Location = new System.Drawing.Point(183, 9);
+            this.btn_guess.Name = "btn_guess";
+            this.btn_guess.Size = new System.Drawing.Size(69, 55);
+            this.btn_guess.TabIndex = 3;
+            this.btn_guess.Text = "make guess";
+            this.btn_guess.UseVisualStyleBackColor = true;
+            this.btn_guess.Click += new System.EventHandler(this.btn_guess_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(319, 453);
+            this.Controls.Add(this.btn_guess);
+            this.Controls.Add(this.lbl_messaggio);
+            this.Controls.Add(this.txt_guess);
+            this.Controls.Add(this.lbl_parola);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_parola;
+        private System.Windows.Forms.TextBox txt_guess;
+        private System.Windows.Forms.Label lbl_messaggio;
+        private System.Windows.Forms.Button btn_guess;
+    }
+}
+
---
+++ es/es/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace es
+{
+    public partial class Form1 : Form
+    {
+        string[] parole = new string[200];
+        int tentativi = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            string elenco = @"parole.txt";
+            using (StreamReader sr = new StreamReader(elenco))
+            {
+                int cont = 0;
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    parole[cont] = line;
+                    cont++;
+                }
+            }
+            Random rand = new Random(Environment.TickCount);
+            int num = rand.Next(0, 200);
+            MessageBox.Show("La parola è: " + parole[num]);
+            displaypar(parole[num], "");
+        }
+
+        private void displaypar(string parola, string lettereindovinate)
+        {
+            parola = parola.ToUpper();
+            string lett1 = parola[0].ToString();
+
+            int c = 0;
+                for (int i = 1; i < parola.Length - 1; i++)
+                {
+                    lett1 += "_ ";
+                }
+
+            char[] lettind = lettereindovinate.ToCharArray();
+            char[] temp = lett1.ToCharArray();
+
+            for (int i = 1; i < parola.Length; i++)
+            {
+                if (parola[i] == lettereindovinate[c])
+                {
+                    temp = lettereindovinate;
+                }
+            }
+            /*}
+            else
+            {
+                for(int i = 0; i < lettereindovinate.Length; i++)
+                {
+                    for(int j = 1; j < parola.Length - 1; i++)
+                    {
+                        if()
+                        {
+
+                        }
+                    }
+                }
+            }*/
+            lett1 += parola[parola.Length - 1].ToString();
+            lbl_parola.Text = lett1;
+        }
+
+        private bool controlla(string tentativo)
+        {
+            string guess = lbl_parola.Text;
+            string lettereindovinate = "";
+            if (tentativo != guess)
+            {
+                for(int i = 1; i < guess.Length - 1; i ++)
+                {
+                    if (tentativo[i] == guess[i])
+                    {
+                        lettereindovinate += tentativo[i].ToString();
+                    }
+                }
+                tentativi++;
+                displaypar(guess, lettereindovinate);
+                return false;
+            } else
+            {
+                return true;
+            }
+        }
+
+        private void btn_guess_Click(object sender, EventArgs e)
+        {
+            string tentativo = txt_guess.Text.ToUpper();
+
+            if(controlla(tentativo))
+            {
+                MessageBox.Show($"Hai indovinato la parola in {tentativi}");
+            }
+        }
+    }
+}
